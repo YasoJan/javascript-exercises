036388e22001075e29565a695edd2cc922f0e85f
yasojan <yasinzahir98@gmail.com>
2026-03-04 14:15:18 -0800

    Reverse for loop

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  str_1 = "";
+  for(let i = str.length-1; i >= 0; i--){
+    str_1+=str[i];
+  }
+  return str_1;
 };
 
 // Do not edit below this line
