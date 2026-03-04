5318a1fe208839090631ca7d862c571d7452442e
yasojan <yasinzahir98@gmail.com>
2026-03-04 09:44:50 -0800

    Solve algorithmic challenge

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+function repeatString(str, num) {
+  if(num < 0){
+    return "ERROR";
+  }
+  str1 = ""
+  for(let i =0; i<num; i++){
+    str1+=str;
+  }
+return str1;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
