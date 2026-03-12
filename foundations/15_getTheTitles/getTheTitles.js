d94387b59abc70ea29910571fbe14a49eb4b02fc
yasojan <yasinzahir98@gmail.com>
2026-03-11 20:14:24 -0700

    Finish the challenge

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,28 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  return arr.reduce((result, item) =>{
+    result.push(item.title)
+    return result;
+  }, []);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/*
+You are given an array of objects that represent books with an author and a title that looks like this:
+
+```javascript
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+```
+
+Your job is to write a function that takes the array and returns an array of titles:
+*/
