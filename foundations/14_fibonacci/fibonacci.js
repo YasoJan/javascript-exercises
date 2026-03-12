f3c0b40b623df4c09589bc17b8d9130d68085431
yasojan <yasinzahir98@gmail.com>
2026-03-11 20:05:40 -0700

    Solve problem

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(typeof num === "string"){
+    num = parseInt(num);
+  }
+  if(num < 0){
+    return "OOPS";
+  }
+  if(num === 0){
+    return 0;
+  }
+  let arr = [1, 1];
+  for(let i = 1; i<num; i++){
+    arr.push(arr[i] + arr[i-1]);
+  }
+  return arr[num-1];
 };
 
 // Do not edit below this line
