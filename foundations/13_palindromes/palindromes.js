66be8ec008e784dc897976ef0d00a6aaa72c178b
yasojan <yasinzahir98@gmail.com>
2026-03-11 18:59:02 -0700

    Use replaceAll()

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  str = str.replaceAll(",", "").replaceAll(" ", "").replaceAll(".", "").replaceAll("!","").toLowerCase();
+  
+  let reversedStr = str.split("").reverse().join("");
 
+  return str === reversedStr;
 };
 
 // Do not edit below this line
